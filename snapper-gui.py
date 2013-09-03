739ecb70b1de0cc824cf1e1170dc9e3ca3736fbe
Ricardo Vieira <ricardomarcelinovieira@gmail.com>
2013-09-03 02:06:35 +0100

    change create snapshot options like config description and cleanup

--- snapper-gui.py
+++ snapper-gui.py
@@ -118,10 +118,27 @@ class createDialog(object):
 		self.dialog.set_transient_for(parent)
 		builder.connect_signals(self)
 
-		self.config = "root"
-		self.description = "test class"
-		self.cleanup = "number"
+		self.config = ""
+		self.description = ""
+		self.cleanup = ""
 		self.userdata = {"by":"SnapperGui"}
+
+		configscombo = Gtk.ListStore(str)
+		for config in snapper.ListConfigs():
+			configscombo.append( [str(config[0])] )
+
+		combobox = builder.get_object("configsCombo")
+		combobox.set_model(configscombo)
+		combobox.set_active(0)
+
+	def on_config_changed(self,widget):
+		self.config = widget.get_model()[widget.get_active()][0]
+
+	def on_description_changed(self,widget):
+		self.description = widget.get_chars(0,-1)
+
+	def on_cleanup_changed(self,widget):
+		self.cleanup = widget.get_chars(0,-1)
 
 	def run(self):
 		return self.dialog.run()
