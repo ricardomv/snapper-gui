8caddd9d83c75d07f47f7888a1fa1a28543a7067
ricardo-vieira <ricardo.vieira@ist.utl.pt>
2013-09-23 00:38:37 +0100

    display unified diff of selected file in changes window

--- snappergui/changesWindow.py
+++ snappergui/changesWindow.py
@@ -2,9 +2,10 @@
 import pkg_resources
 import dbus
 import os
-import stat
+import time
+import difflib
 from dbus.mainloop.glib import DBusGMainLoop
-from gi.repository import Gtk, Gdk#, GObject
+from gi.repository import Gtk, Gdk, GObject, GtkSource
 
 bus = dbus.SystemBus(mainloop=DBusGMainLoop())
 snapper = dbus.Interface(bus.get_object('org.opensuse.Snapper', '/org/opensuse/Snapper'),
@@ -15,41 +16,55 @@ class changesWindow(object):
 	def __init__(self, config, begin, end):
 		super(changesWindow, self).__init__()
 		builder = Gtk.Builder()
+		GObject.type_register(GtkSource.View)
 		builder.add_from_file(pkg_resources.resource_filename("snappergui", "glade/changesWindow.glade"))
 		
 		self.window = builder.get_object("changesWindow")
 		treeview = builder.get_object("pathstreeview")
+		diffview = builder.get_object("diffview")
+
 		self.window.show_all()
-		
-		#builder.connect_signals(self)
+
+		self.beginpath = snapper.GetMountPoint(config, begin)
+		self.endpath = snapper.GetMountPoint(config, end)
+
+		manager = GtkSource.LanguageManager()
+		language = manager.get_language("diff")
+
+		self.diffbuffer = GtkSource.Buffer.new_with_language(language)
+
+		diffview.set_buffer(self.diffbuffer)
+		builder.connect_signals(self)
 
 		snapper.CreateComparison(config,begin,end)
 		dbus_array = snapper.GetFiles(config,begin,end)
-		paths_list = []
-		for path in dbus_array:
-			paths_list.append(str(path[0]))
 
 		files_tree = {}
-		for path in paths_list:
-			self.add_path_to_tree(path,files_tree)
+		for path in dbus_array:
+			self.add_path_to_tree(str(path[0]),files_tree)
 
-		builder.get_object("statusbar1").push(True,"%d files"%len(paths_list))
+		builder.get_object("statusbar1").push(True,"%d files"%len(dbus_array))
 
 		
 		treeview.set_model(self.get_treestore_from_tree(files_tree))
-		treeview.expand_all()
+		#treeview.expand_all()
 
 		snapper.DeleteComparison(config,begin,end)
 
+
 	def add_path_to_tree(self, path, tree):
+		is_dir = os.path.isdir(path)
 		parts = path.split('/')
 		node = tree
-		for file_name in parts:
-			if not file_name == parts[-1]:
-				file_name += "/"
+		for file_name in parts[0:-1]:
+			file_name += '/'
 			if not file_name in node:
 				node[file_name] = {}
 			node = node[file_name]
+		if is_dir:
+			node[parts[-1]+'/'] = {}
+		else:
+			node[parts[-1]] = path
 
 	def print_tree(self, tree, tabs=""):
 		tabs += "\t"
@@ -58,10 +73,39 @@ class changesWindow(object):
 			self.print_tree(files,tabs)
 
 	def get_treestore_from_tree(self, tree):
-		treestore = Gtk.TreeStore(str, str)
+		treestore = Gtk.TreeStore(str, str, str)
 		def get_childs(tree, parent=None):
 			for path, childs in tree.items():
-				node = treestore.append(parent,[Gtk.STOCK_FILE, path])
+				node = treestore.append(parent,[
+					Gtk.STOCK_DIRECTORY if "/" in path else Gtk.STOCK_FILE, 
+					path, 
+					childs if type(childs) == str else ""
+					])
+				if type(childs) == str: continue
 				get_childs(childs,node)
 		get_childs(tree)
 		return treestore
+
+	def _on_pathstree_selection_changed(self, selection):
+		(model, treeiter) = selection.get_selected()
+		if treeiter != None:
+			fromfile = self.beginpath+model[treeiter][2]
+			tofile = self.endpath+model[treeiter][2]
+			try:
+				fromlines = list(open(fromfile))
+				fromdate = time.ctime(os.stat(fromfile).st_mtime)
+			except FileNotFoundError:
+				fromfile = "New file"
+				fromlines = ""
+				fromdate = ""
+
+			try:
+				tolines = list(open(tofile,"r"))
+				todate = time.ctime(os.stat(tofile).st_mtime)
+			except FileNotFoundError:
+				tofile = "Deleted file"
+				tolines = ""
+				todate = ""
+			difflines = difflib.unified_diff(fromlines, tolines, fromfile=fromfile, tofile=tofile, fromfiledate=fromdate, tofiledate=todate)
+			difftext = "".join(difflines)
+			self.diffbuffer.set_text(difftext)
