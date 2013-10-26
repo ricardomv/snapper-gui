84883d2257cc440fc10901e6db84989a302355f2
Ricardo Vieira <ricardomarcelinovieira@gmail.com>
2013-10-26 14:55:04 +0100

    open config properties from application menu

--- snappergui/application.py
+++ snappergui/application.py
@@ -1,6 +1,7 @@
 import pkg_resources, sys, signal
 from gi.repository import Gtk, GLib, Gdk, GdkPixbuf, Gio#, GObject
 from snappergui.mainWindow import SnapperGUI
+from snappergui.propertiesDialog import propertiesDialog
 
 def start_ui():
 	app = Application()
@@ -24,6 +25,10 @@ class Application(Gtk.Application):
 		menu = builder.get_object('app-menu')
 		self.set_app_menu(menu)
 
+		propertiesAction = Gio.SimpleAction.new('properties', None)
+		propertiesAction.connect('activate', self.show_configs_properties)
+		self.add_action(propertiesAction)
+
 		aboutAction = Gio.SimpleAction.new('about', None)
 		aboutAction.connect('activate', self.about)
 		self.add_action(aboutAction)
@@ -31,6 +36,11 @@ class Application(Gtk.Application):
 		quitAction = Gio.SimpleAction.new('quit', None)
 		quitAction.connect('activate', self.quit)
 		self.add_action(quitAction)
+
+	def show_configs_properties(self, action, param):
+		dialog = propertiesDialog(self)
+		dialog.dialog.run()
+		dialog.dialog.hide()
 
 	def about(self, action, param):
 		pass
