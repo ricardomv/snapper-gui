37165f7219d246d90ed82795b9ffbf38261412e5
Ricardo Vieira <ricardo.vieira@tecnico.ulisboa.pt>
2016-01-13 13:35:41 +0000

    Use gi.require_version to silence gtk warning

--- snappergui/__init__.py
+++ snappergui/__init__.py
@@ -1,5 +1,8 @@
 import dbus
 from dbus.mainloop.glib import DBusGMainLoop
+import gi
+gi.require_version("Gtk", "3.0")
+gi.require_version("GtkSource", "3.0")
 
 bus = dbus.SystemBus(mainloop=DBusGMainLoop())
 snapper = dbus.Interface(bus.get_object('org.opensuse.Snapper',
