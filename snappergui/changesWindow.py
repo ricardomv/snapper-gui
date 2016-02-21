99d112952e0f31a5296da9091ac9f2c3e05692d4
Marek Rusinowski <marekrusinowski@gmail.com>
2016-02-21 19:28:01 +0100

    Improve changes window
    - Add colors and tooltips indicating what happened to file between
      snapshots
    - Fix handling of new directories
    - Remove one level from tree view

--- snappergui/changesWindow.py
+++ snappergui/changesWindow.py
@@ -1,9 +1,28 @@
 from snappergui import snapper
-import pkg_resources, os, time, difflib
-from gi.repository import Gtk, GtkSource, GObject
+import pkg_resources, os, time, difflib, collections
+from gi.repository import Gtk, GtkSource, GObject, Gdk
+
+
+class StatusFlags(object):
+    """File status flags from https://github.com/openSUSE/snapper/blob/master/snapper/File.h#L39-L51"""
+
+    CREATED = 1        # created
+    DELETED = 2        # deleted
+    TYPE = 4           # type has changed
+    CONTENT = 8        # content has changed
+    PERMISSIONS = 16   # permissions have changed, see chmod(2)
+    OWNER = 32         # owner has changed, see chown(2)
+    USER = 32          # deprecated - alias for OWNER
+    GROUP = 64         # group has changed, see chown(2)
+    XATTRS = 128       # extended attributes changed, see attr(5)
+    ACL = 256          # access control list changed, see acl(5)
+
 
 class changesWindow(object):
     """docstring for changesWindow"""
+
+    TreeNode = collections.namedtuple("TreeNode", "path, children, status, is_dir")
+
     def __init__(self, config, begin, end):
         super(changesWindow, self).__init__()
         builder = Gtk.Builder()
@@ -34,42 +53,85 @@ class changesWindow(object):
         self.window.show_all()
         GObject.idle_add(self.on_idle_init_paths_tree)
 
-    def add_path_to_tree(self, path, tree):
-        is_dir = os.path.isdir(path)
+    def add_path_to_tree(self, path, status, tree):
+        is_dir = os.path.isdir(self.beginpath + path) or os.path.isdir(self.endpath + path)
         parts = path.split('/')
         node = tree
         # Add directories to tree
-        for file_name in parts[0:-1]:
-            file_name += '/'
-            if not file_name in node:
-                node[file_name] = {}
-            node = node[file_name]
+        for file_name in parts[1:-1]:
+            if not file_name in node.children:
+                node.children[file_name] = changesWindow.TreeNode("", {}, 0, True)
+            node = node.children[file_name]
         # Add last part of path to tree
         if is_dir:
-            node[parts[-1]+'/'] = {} # create new node
+            node.children[parts[-1]] = changesWindow.TreeNode("", {}, status, True)
         else:
-            node[parts[-1]] = path # save the path
+            node.children[parts[-1]] = changesWindow.TreeNode(path, None, status, False)
 
-    def print_tree(self, tree, tabs=""):
-        for path, files in tree.items():
-            print(tabs+path)
-            if type(files) == str: continue
-            self.print_tree(files,tabs+"\t")
+    def print_tree(self, tree, indent=""):
+        for name, child in tree.children.items():
+            print(indent + name + ("/" if child.is_dir else "") + "   (" +
+                  self.file_status_to_string(child.status) + ")")
+            if child.is_dir:
+                self.print_tree(child, indent + "    ")
+
+    def file_status_to_string(self, status):
+        if status & StatusFlags.CREATED:  # Created file
+            return "Created"
+        elif status & StatusFlags.DELETED:  # Deleted file
+            return "Deleted"
+        elif status > 0:  # Modified file
+            modification_types = {
+                StatusFlags.ACL: "acl",
+                StatusFlags.CONTENT: "content",
+                StatusFlags.GROUP: "group",
+                StatusFlags.OWNER: "owner",
+                StatusFlags.XATTRS: "xattrs",
+                StatusFlags.TYPE: "inode type",
+                StatusFlags.PERMISSIONS: "permissions"
+            }
+            modified_list = []
+            for flag, name in modification_types.items():
+                if status & flag:
+                    modified_list.append(name)
+            return "Modified: " + ", ".join(modified_list)
+        else:
+            return "No changes"
 
     def get_treestore_from_tree(self, tree):
-        # Row: [gtk-stock-icon, file name, file complete path]
-        treestore = Gtk.TreeStore(str, str, str)
+        # Row: [gtk-stock-icon, file name, file complete path, entry color, tooltip]
+        treestore = Gtk.TreeStore(str, str, str, Gdk.RGBA, str)
         def get_childs(tree, parent=None):
-            for path, childs in tree.items():
+            for file_name, child in tree.children.items():
+                color = Gdk.RGBA(0.0, 0.0, 0.0)
+                if child.status & StatusFlags.CREATED:  # Created file
+                    color = Gdk.RGBA(0.0, 0.57, 0.0)
+                elif child.status & StatusFlags.DELETED:  # Deleted file
+                    color = Gdk.RGBA(0.6, 0.0, 0.0)
+                elif child.status > 0:  # Modified file
+                    color = Gdk.RGBA(0.49, 0.47, 0.0)
+
                 node = treestore.append(parent,[
-                    Gtk.STOCK_DIRECTORY if "/" in path else Gtk.STOCK_FILE,
-                    path, childs if type(childs) == str else ""
+                    Gtk.STOCK_DIRECTORY if child.is_dir else Gtk.STOCK_FILE,
+                    file_name, child.path, color, self.file_status_to_string(child.status)
                     ])
                 # if this child is a directory get childs
-                if not type(childs) == str:
-                    get_childs(childs,node)
+                if child.children is not None:
+                    get_childs(child, node)
         get_childs(tree)
         return treestore
+
+    def on_query_tooltip(self, widget, x, y, keyboard_tip, tooltip):
+        if not widget.get_tooltip_context(x, y, keyboard_tip):
+            return False
+        else:
+            on_row, x, y, model, path, it = widget.get_tooltip_context(x, y, keyboard_tip)
+            if on_row:
+                tooltip.set_text(model[it][4])
+                widget.set_tooltip_row(tooltip, path)
+                return True
+            else:
+                return False
 
     def on_idle_init_paths_tree(self):
         snapper.CreateComparison(self.config,self.snapshot_begin,self.snapshot_end)
@@ -77,9 +139,9 @@ class changesWindow(object):
         dbus_array = snapper.GetFiles(self.config,self.snapshot_begin,self.snapshot_end)
 
         # create structure to sort paths into tree
-        files_tree = {}
-        for path in dbus_array:
-            self.add_path_to_tree(str(path[0]),files_tree)
+        files_tree = changesWindow.TreeNode("/", {}, 0, True)
+        for entry in dbus_array:
+            self.add_path_to_tree(str(entry[0]), int(entry[1]), files_tree)
 
         #self.print_tree(files_tree)
         self.pathstreeview.set_model(self.get_treestore_from_tree(files_tree))
